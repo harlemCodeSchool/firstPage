ad2bbca75d50dd1a3bdc9eb3a1c064aa824a7218
ttante <tylerdinner@gmail.com>
2014-09-22 00:30:47 -0400

    make over

--- js.js
+++ js.js
@@ -1,22 +1,21 @@
 window.onload = function () {
 	console.log('js.js reporting for duty!');
-/*
-	var makeBox = document.createElement('div');
-	var styleDiv = makeBox.style();
-    styleDiv.height = ('45px');
-    styleDiv.width = ('45px');
-    styleDiv.border = ('2px solid red');
-*/
 
 	var x = 5;
 	var y = 10;
 
 	var adder = function(a, b) {
+		
 		total = a + b;
 		return total;
 	}
 
+
 	adder(x, y);
+
+
+
+
 
     var bandRater = function(band, power, soul, taste){
     	if ((power + soul + taste) <= 6) {
